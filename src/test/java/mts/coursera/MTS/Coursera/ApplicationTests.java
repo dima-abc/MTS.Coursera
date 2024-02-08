39d52e0d9d8d111ac30e7439be29ebf541571b1e
Dima-Stepanov <haoos@inbox.ru>
2024-02-08 15:07:47 +0300

    firs commit

---
+++ src/test/java/mts/coursera/MTS/Coursera/ApplicationTests.java
@@ -0,0 +1,13 @@
+package mts.coursera.MTS.Coursera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
